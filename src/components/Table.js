1d55796f29edaa0cc5a4f6dffc01e92ddd072cbb
Lyle Rubin <lylerubin912@gmail.com>
2021-05-23 09:22:29 -0800

    Table now shows csv contents

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -6,7 +6,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Grid from "@material-ui/core/Grid";
-import Papa from 'papaparse'
+import Papa from "papaparse";
 
 const useStyles = makeStyles({
   table: {
@@ -14,37 +14,22 @@ const useStyles = makeStyles({
   },
 });
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
 export default function DenseTable() {
-  const [rowsCSV, setRows] = React.useState([])
+  const [rows, setRows] = useState([]);
 
-  React.useEffect(() => {
+  useEffect(() => {
     async function getData() {
-      const response = await fetch('/data/red_flag_schedule.csv')
-      const reader = response.body.getReader()
-      const result = await reader.read() // raw array
-      const decoder = new TextDecoder('utf-8')
-      const csv = decoder.decode(result.value) // the csv text
-      const results = Papa.parse(csv, { header: true }) // object with { data, errors, meta }
-      const rwos = results.data // array of objects
-      console.log(rwos)
-      setRows(rwos)
+      const response = await fetch("/data/red_flag_schedule.csv");
+      const reader = response.body.getReader();
+      const result = await reader.read();
+      const decoder = new TextDecoder("utf-8");
+      const csv = decoder.decode(result.value);
+      const results = Papa.parse(csv, { header: true });
+      setRows(results.data);
+      console.log(results.data);
     }
-    getData()
-  }, []) // [] means just do this once, after initial render
+    getData();
+  }, []);
 
   const classes = useStyles();
 
@@ -53,23 +38,19 @@ export default function DenseTable() {
       <Table className={classes.table} size="small" aria-label="a dense table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Start</TableCell>
+            <TableCell>End</TableCell>
+            <TableCell>Event</TableCell>
+            <TableCell>Location</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+            <TableRow>
+              <TableCell>{row.Start}</TableCell>
+              <TableCell>{row.End}</TableCell>
+              <TableCell>{row.Event}</TableCell>
+              <TableCell>{row.Location}</TableCell>
             </TableRow>
           ))}
         </TableBody>
