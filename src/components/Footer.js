ca985221770b0b0a6240cac073325964de0f616e
Lyle Rubin <lylerubin912@gmail.com>
2021-06-06 19:35:14 -0800

    Updated to MSOE v3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,15 @@
-import { Typography } from '@material-ui/core'
-import React from 'react'
+import { Typography } from "@material-ui/core";
+import React from "react";
 
 export default function Footer() {
   return (
     <Typography
-    className="footer"
-    variant="subtitle2"
-    align="center"
-    gutterBottom
-  >
-    Schedule App Created by SSgt Lyle Rubin, 353 CTS
-  </Typography>
-  )
+      className="footer"
+      variant="subtitle2"
+      align="center"
+      gutterBottom
+    >
+      Schedule App Created by SSgt Lyle Rubin, 353 CTS
+    </Typography>
+  );
 }
